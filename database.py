780da01deee9d293c2bd89ef9a4f782ee3929fea
Shashank Rayarao <shashankrayarao@gmail.com>
2026-09-25 15:34:45 +0530

    Refactor database connection setup
    Updated database URL to hide password.

--- database.py
+++ database.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-database_url = "mysql+pymysql://root:2004@localhost:3306/fastapi_db"
+database_url = "mysql+pymysql://root:****/fastapi_db"
 engine = create_engine(database_url)
 session = sessionmaker(autocommit = False, autoflush = False, bind = engine)
